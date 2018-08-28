4027d05b38e9f78ea60d4a11aef8bcfc0b9e444c
Ahmad Kayyali <kayyali18@gmail.com>
2018-08-28 13:28:12 -0600

    Set up node and trie classes

---
+++ test/node-test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import Node from '../scripts/node';
+
+describe('NODE', () => {
+  let node;
+
+  beforeEach(() => {
+    node = new Node('p');
+  })
+
+  it('should exist', () => {
+    expect(node).to.exist;
+  })
+
+  it('should take letter as argument and assign it to the letter property', () => {
+    expect(node.letter).to.equal('p');
+  })
+
+  it('should default end to false', () => {
+    expect(node.end).to.equal(false)
+  })
+
+  it('should default children to null', () => {
+    expect(node.children).to.equal(null);
+  })
+})
